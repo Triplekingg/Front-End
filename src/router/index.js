1f54c98ec21973ea295bb4896436156377edc57c
Rathanon <rathanon.sin@student.mahidol.edu>
2021-07-19 01:33:39 +0700

    Configure frontend proxy to backend api

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,49 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+// import store from "@/store";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/login",
-    name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+    },
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
+});
+
+// Setup beforeEach hook to check the logged in synce the loggin states with backend
+router.beforeEach(async (to, from, next) => {
+    // get login state using whoami and exios
+    let response = await Vue.axios.get("/api/whoami");
+
+    console.log(response);
+    if (to.name !== 'Login') {
+        next({name: 'Login'});
+    } else next();
 });
 
 export default router;
