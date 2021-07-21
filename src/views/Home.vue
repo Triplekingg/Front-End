a670c12c90fac1379b999e02478bb5bac70df817
Manit Gandhi <yourgitemail@example.com>
2021-07-21 17:01:20 +0700

    -change home page features(remove logout, convert links of "go to log in", "go to sign up" to buttons. -Remove Log Out button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,29 @@
 <template>
   <v-container>
     <h2>Home</h2>
+    <h3>Welcome to Good Stuff game reviews. Please Log in or Sign Up to Continue</h3>
     <ul>
-      <li>
-       <router-link to="/">Go to Home</router-link>
-      </li>
-      <li>
-        <router-link to="/about">Go to About</router-link>
-      </li>
-      <li>
-        <router-link to="/login">Go to Login</router-link>
-      </li>
+<!--      <li>-->
+<!--       <router-link to="/">Go to Home</router-link>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <router-link to="/about">Go to About</router-link>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <router-link to="/login">Go to Login</router-link>-->
+<!--      </li>-->
 <!--      <li>-->
 <!--        <router-link to="/signup">Go to Sign up</router-link>-->
 <!--      </li>-->
+
+      <v-btn
+        color="success"
+        class="mr-4"
+        @click="redirectionLogin"
+      >
+        Log in
+      </v-btn>
+
       <v-btn
         color="success"
         class="mr-4"
@@ -22,13 +32,13 @@
         Sign Up
       </v-btn>
 
-      <v-btn
-        color="success"
-        class="mr-4"
-        @click="logout"
-      >
-        Logout
-      </v-btn>
+<!--      <v-btn-->
+<!--        color="success"-->
+<!--        class="mr-4"-->
+<!--        @click="logout"-->
+<!--      >-->
+<!--        Logout-->
+<!--      </v-btn>-->
     </ul>
   </v-container>
 </template>
@@ -40,6 +50,8 @@ export default {
   methods: {
     async redirection(){
       this.$router.push({ path: "/signup" });
+    },async redirectionLogin(){
+      this.$router.push({ path: "/login" });
     },
     async logout() {
       //submit to backend to logout
